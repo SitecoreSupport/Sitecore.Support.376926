a90f0a99409ca2a24131eb99ea165f4ee116fbdc
akrupskiy <anr@sitecore.net>
2019-11-27 11:48:40 +0200

    import code

---
+++ src/Sitecore.Support.376926/Modules/EmailCampaign/Core/Crypto/AuthenticatedAesStringCipher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Sitecore.Diagnostics;
+using Sitecore.ExM.Framework.Diagnostics;
+using Sitecore.Modules.EmailCampaign.Core.Crypto;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.Crypto
+{
+    public class AuthenticatedAesStringCipher : Sitecore.Modules.EmailCampaign.Core.Crypto.AuthenticatedAesStringCipher, IStringCipher
+    {
+        public AuthenticatedAesStringCipher(byte[] cryptographicKey, byte[] authenticationKey, ILogger logger) : base(cryptographicKey, authenticationKey, logger)
+        {
+        }
+        public AuthenticatedAesStringCipher(string cryptographicKeyName, string authenticationKeyName, ILogger logger) : base(cryptographicKeyName, authenticationKeyName, logger)
+        {
+        }
+
+        string IStringCipher.TryDecrypt(string encryptedMessage)
+        {
+            byte[] buffer;
+            Assert.ArgumentNotNull(encryptedMessage, "encryptedMessage");
+            try
+            {
+                buffer = System.Convert.FromBase64String(encryptedMessage);
+            }
+            catch (FormatException)
+            {
+                return null;
+            }
+
+            return base.TryDecrypt(encryptedMessage);
+        }
+
+    }
+}
---
+++ src/Sitecore.Support.376926/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.376926")]
+[assembly: AssemblyProduct("Sitecore.Support.376926")]
+[assembly: ComVisible(false)]
